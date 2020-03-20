51c05a785ccdf2f77b370241bc3c234bf2b2307f
Krish Kshir <krishkshir@users.noreply.github.com>
2020-03-19 21:45:01 -0700

    Splitting off net worth computation
    ... in it's own separate python script
    Also computing grouped dataframes explicitly in main rather than in
    write_reports

--- budget_analysis.py
+++ budget_analysis.py
@@ -74,23 +74,21 @@ def categorize_tx(a_tx, a_cols=['Category', 'Amount']):
     return grouped_tx
 # enddef categorize_tx() #
 
-def write_reports(a_report_file, a_period, a_prev_bal, a_inc, a_exp,
-        a_summary_file):
+def write_reports(a_report_file, a_period, a_grp_inc, a_grp_exp, a_summary_file):
     """ Function to create summary reports.
     Parameters:
         a_report_file (str): Filename of where to store reports for
         a_period (str): Time period for which transactions are processed
-        a_prev_bal (float): Previous balance
-        a_inc (Dataframe): Dataframe containing only income information
-        a_exp (Dataframe): Dataframe containing only expense information
+        a_grp_inc (Dataframe): Dataframe containing grouped income information
+        a_grp_exp (Dataframe): Dataframe containing grouped expense information
         a_summary_file (str): Filename where summaries of previous runs
             are stored
     Returns:
         None
     """
     # compute total income and expenses
-    tot_inc = np.sum(a_inc['Amount'])
-    tot_exp = np.sum(a_exp['Amount'])
+    tot_inc = np.sum(a_grp_inc['Amount'])
+    tot_exp = np.sum(a_grp_exp['Amount'])
     # compute savings, savings-% and net worth
     savings = tot_inc - tot_exp
     # NOTE: We implicitly assume total income > 0, otherwise the computation of
@@ -103,38 +101,31 @@ def write_reports(a_report_file, a_period, a_prev_bal, a_inc, a_exp,
     else:
         sav_pct = 100.0 * savings / tot_inc
     # endif #
-    net_worth = a_prev_bal + savings
     # compute and plot grouped expenses and income
-    grp_inc = categorize_tx(a_inc)
-    grp_exp = categorize_tx(a_exp)
     with open(a_report_file,'w') as rf:
         rf.write("Budget Report for {}\n".format(a_period))
-        rf.write("Starting balance = $ {:.2f}\n".format(a_prev_bal))
         rf.write("Total income = $ {:.2f}\n".format(tot_inc))
         rf.write("Total expenses = $ {:.2f}\n".format(tot_exp))
         rf.write("Net savings = $ {:.2f}\n".format(savings))
-        rf.write("Net worth = $ {:.2f}\n".format(net_worth))
         rf.write("Savings as a % of income = {:.2f}%\n".format(sav_pct))
         rf.write("\nCategory-wise income [$]:\n")
-        rf.write(grp_inc.to_string())
+        rf.write(a_grp_inc.to_string())
         rf.write("\n\nCategory-wise expenses [$]:\n")
-        rf.write(grp_exp.to_string())
+        rf.write(a_grp_exp.to_string())
     # endwith #
     if not os.path.exists(a_summary_file):
         with  open(a_summary_file,'w') as sf:
-            sf.write("Time Period,Income [$],Expenses [$],Savings [$],"+
-                    "Net Worth [$],pct-savings [%]\n")
+            sf.write("Time period,Income [$],Expenses [$],Savings [$],"+
+                    "pct-savings [%]\n")
         # endwith #
     # endif #
     with open(a_summary_file,'a') as sf:
-        sf.write("{},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n".format(a_period,
-            tot_inc, tot_exp, savings, net_worth, sav_pct))
+        sf.write("{},{:.2f},{:.2f},{:.2f},{:.2f}\n".format(a_period,
+            tot_inc, tot_exp, savings, sav_pct))
     # endwith #
-    return grp_inc, grp_exp
 # enddef write_reports() #
 
-def plot_tallied_tx(a_grouped_tx, a_title, a_plotfile,
-        a_col='Contribution [%]'):
+def plot_tallied_tx(a_grouped_tx, a_title, a_plotfile, a_col='Contribution [%]'):
     """
     Function to plot tallied transactions
     Paramaters:
@@ -158,45 +149,14 @@ def plot_tallied_tx(a_grouped_tx, a_title, a_plotfile,
     plt.savefig(a_plotfile)
 # enddef plot_tallied_tx() #
 
-def plot_overall_summary(a_summary_file, a_summary_plot):
-    """
-    Function to plot overall summary as lineplot.
-    Parameters:
-        a_summary_file (str): Filename where summaries of previous runs
-            are stored
-        a_summary_plot (str): Filename where trendline summaries are to be
-            plotted
-    Returns:
-        None
-    """
-    summary_df = pd.read_csv(a_summary_file)
-    plt.close('all')
-    fig, ax1 = plt.subplots()
-    ax1.plot(summary_df.iloc[:,0],summary_df.iloc[:,1:5])
-    ax1.set_ylabel('$')
-    ax1.legend(['Income', 'Expenses', 'Savings', 'Net worth'], loc='upper left')
-    ax2 = ax1.twinx()
-    ax2.plot(summary_df.iloc[:,0], summary_df.iloc[:,5],'k--')
-    ax2.set_ylabel('%')
-    ax2.legend(['%-savings'], loc='upper right')
-    ax1.minorticks_on()
-    ax1.grid(b=True, which='both', axis='both', color='r', linestyle='-',
-            linewidth=0.2)
-    ax2.minorticks_on()
-    ax2.grid(b=True, which='both', axis='both', color='b', linestyle='--',
-            linewidth=0.3)
-    plt.tight_layout()
-    plt.savefig(a_summary_plot)
-# enddef plot_overall_summary() #
-
-def main(a_tx_file, a_period, a_report_file, a_prev_bal=0.0,
-        a_summary_file="MyBudget2019.csv", a_summary_plot="TallyTrend.png"):
+def main(a_tx_file, a_period="Test period", a_report_file="Test_report.txt",
+        a_summary_file="MyBudget_summary.csv",
+        a_summary_plot="MyBudgetSummaryPlot.png"):
     """ Main function.
     Parameters:
         a_tx_file (str): Name of transactions CSV file
         a_period (str): Time period for which transactions are processed
         a_report_file (str): Filename of where to store reports for
-        a_prev_bal (float): Previous balance
         a_summary_file (str): Filename where summaries of previous runs
             are stored
         a_summary_plot (str): Filename where trendline summaries are to be
@@ -204,25 +164,20 @@ def main(a_tx_file, a_period, a_report_file, a_prev_bal=0.0,
     Returns:
         None
     """
-    assert isinstance(a_prev_bal, (float, int)),\
-        "Previous balance must be numeric."
+    # read the transaction file
     exp, inc = read_tx_file(a_tx_file)
-    # compute total expenses, income, savings, %-savings, net worth and write
-    # summary reports
-    grp_inc, grp_exp = write_reports(a_report_file, a_period, a_prev_bal, inc,
-            exp, a_summary_file)
+    # categorize expenses and income
+    grp_inc = categorize_tx(inc)
+    grp_exp = categorize_tx(exp)
+    # compute total expenses, income, savings, %-savings, and write reports
+    write_reports(a_report_file, a_period, grp_inc, grp_exp, a_summary_file)
     # plot category breakdown for this period and overall summary
     plot_tallied_tx(grp_inc, "Income", "Plot_Income_" + a_period + ".png")
     plot_tallied_tx(grp_exp, "Expenses", "Plot_Expenses_" + a_period + ".png")
-    plot_overall_summary(a_summary_file, a_summary_plot)
 # enddef main() #
 
 if __name__ == "__main__":
     passed_args = sys.argv[1:]
-    if len(passed_args) > 3:
-        # convert string to float
-        passed_args[3] = float(passed_args[3])
-        # endif #
     main(*passed_args)
     # endif #
 # endif #
---
+++ collate_periods.py
@@ -0,0 +1,148 @@
+#!/anaconda3/bin/python
+"""
+Author: Shrikant Kshirsagar
+Purpose: To summarize year-to-date budget from tallied monthly budget sheets
+License: GPLv3+
+"""
+
+import sys
+import warnings
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def summarize_all_periods(a_initial_net_worth, a_summary_file,
+        a_inc_colname="Income [$]", a_exp_colname="Expenses [$]",
+        a_sav_colname="Savings [$]"):
+    """ Summarize YTD results from monthly summary file
+    Parameters:
+        a_initial_net_worth (float): Net worth at beginning of year
+        a_summary_file (DataFrame): Filename containing period summary for
+            different periods
+        a_inc_colname (str): Column name of income column
+        a_exp_colname (str): Column name of expenses column
+        a_sav_colname (str): Column name of savings column
+    Returns:
+        summary_df (DataFrame): DataFrame of summary file with "Net worth [$]"
+            column appended
+    """
+    # read summary file
+    summary_df = pd.read_csv(a_summary_file)
+    # compute total income, expenses and savings for all periods
+    total_income = summary_df[a_inc_colname].sum()
+    total_expenses = summary_df[a_exp_colname].sum()
+    total_savings = total_income - total_expenses
+    # if total income < 0, %-savings is undefined
+    if total_income < 0:
+        warning_msg = "Total income is negative (-${:.2f})!"
+        warning_msg = warning_msg.format(a_period, -total_income)
+        warnings.warn(warning_msg)
+        net_sav_pct = -np.inf
+    else:
+        net_sav_pct = 100.0 * total_savings / total_income
+    # endif #
+    with open(a_summary_file,'a') as sf:
+        sf.write("\nTotal,{:.2f},{:.2f},{:.2f},{:.2f}\n".format(
+            total_income, total_expenses, total_savings, net_sav_pct))
+    # endwith #
+    # compute every month's net worth
+    net_worth = [a_initial_net_worth]
+    for sav in summary_df[a_sav_colname]:
+        net_worth +=  [net_worth[-1] + sav]
+    # endfor #
+    net_worth.pop(0)
+    summary_df["Net worth [$]"] = net_worth
+    return summary_df
+# enddef summarize_all_periods() #
+
+def plot_summary_same_axes(a_summary_df, a_summary_plot, a_period_colname,
+        a_plot_cols):
+    """
+    Function to plot overall summary as lineplot with same axes
+    Parameters:
+        a_summary_df (DataFrame): DataFrame containing summary file + net worth
+        a_summary_plot (str): Filename where trendline summaries are to be
+            plotted
+        a_period_colname (str): Column name containing name of period
+        a_plot_cols (list(str)) : List of strings of column names containing
+            quantities to be plotted
+    Returns:
+        None
+    """
+    plt.close('all')
+    fig, ax = plt.subplots()
+    ax.plot(a_summary_df[a_period_colname],a_summary_df[a_plot_cols])
+    ax.set_ylabel('$')
+    ax.legend(a_plot_cols)
+    ax.minorticks_on()
+    ax.grid(b=True, which='both', axis='both')
+    plt.tight_layout()
+    plt.savefig(a_summary_plot)
+# enddef plot_summary_same_axes() #
+
+def plot_summary_diff_axes(a_summary_df, a_summary_plot, a_period_colname,
+        a_plot_cols):
+    """
+    Function to plot overall summary as lineplot with different axes
+    Parameters:
+        a_summary_df (DataFrame): DataFrame containing summary file + net worth
+        a_summary_plot (str): Filename where trendline summaries are to be
+            plotted
+        a_period_colname (str): Column name containing name of period
+        a_plot_cols (tuple(str)) : 2-tuple of strings of column names containing
+            net-worth and %-savings in that order
+    Returns:
+        None
+    """
+    plt.close('all')
+    fig, ax1 = plt.subplots()
+    ax1.plot(a_summary_df[a_period_colname],a_summary_df[a_plot_cols[0]],
+            color='r')
+    ax1.set_ylabel('$')
+    ax1.legend([a_plot_cols[0]], loc='upper left')
+    ax1.minorticks_on()
+    ax1.grid(b=True, which='both', axis='both', color='r', linestyle='-',
+            linewidth=0.2)
+    ax2 = ax1.twinx()
+    ax2.plot(a_summary_df[a_period_colname], a_summary_df[a_plot_cols[1]],
+            color='b')
+    ax2.set_ylabel('%')
+    ax2.legend([a_plot_cols[1]], loc='upper right')
+    ax2.minorticks_on()
+    ax2.grid(b=True, which='both', axis='both', color='b', linestyle='--',
+            linewidth=0.3)
+    plt.tight_layout()
+    plt.savefig(a_summary_plot)
+# enddef plot_summary_diff_axes() #
+
+def main(a_initial_net_worth, a_summary_file,
+        a_inc_exp_plotfile="Plot_incexp_summary.png",
+        a_networth_savingspct_plotfile="Plot_networth_savingspct.png"):
+    """ Main function
+    Parameters:
+        a_initial_net_worth (float): Initial net worth at the beginning of all
+            periods
+        a_summary_file (str): Filename containing period summary for different
+            periods
+        a_inc_exp_plotfile (str): Filename in which to store plot of income and
+            expenses with time period
+        a_networth_savingspct_plotfile (str): Filename in which to store plot of
+            net worth and savings percent with time period
+        Returns:
+            None
+    """
+    assert isinstance(a_initial_net_worth, (float, int)),\
+        "Previous balance must be numeric."
+    summary_df = summarize_all_periods(a_initial_net_worth, a_summary_file)
+    plot_summary_same_axes(summary_df, a_inc_exp_plotfile, "Time period",
+            ["Income [$]", "Expenses [$]"])
+    plot_summary_diff_axes(summary_df, a_networth_savingspct_plotfile,
+            "Time period", ("Net worth [$]", "pct-savings [%]"))
+# enddef main() #
+
+if __name__ == "__main__":
+    passed_args = sys.argv[1:]
+    passed_args[0] = float(passed_args[0])
+    main(*passed_args)
+    # endif #
+# endif #
